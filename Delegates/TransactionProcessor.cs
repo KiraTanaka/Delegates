b3f29006480c95671b4b77d20dc5c2f85c3e3221
Kira Tanaka <suein_i@mail.ru>
2016-10-24 20:45:38 +0500

    Create

---
+++ Delegates/TransactionProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public abstract class TransactionProcessor
+    {
+        Func<TransactionRequest, bool> Check;
+        Func<TransactionRequest, Transaction> Register;
+        Action<Transaction> Save;
+        public Transaction Process(TransactionRequest request)
+        {
+            if (!Check(request))
+                throw new ArgumentException();
+            var result = Register(request);
+            Save(result);
+            return result;
+        }
+    }
+}
