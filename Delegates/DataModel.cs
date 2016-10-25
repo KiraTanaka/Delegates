63c3a04f38107eac5b5bae83b32473ce4d06cf10
Kira Tanaka <suein_i@mail.ru>
2016-10-26 00:43:18 +0500

    Update.Task 3

---
+++ Delegates/DataModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void EventHandler();
+    public class DataModel
+    {
+        public event EventHandler OnPut;
+        public event EventHandler OnInsertRow;
+        public event EventHandler OnInsertColumn;
+
+        public void Put(int row, int column, int value)
+        {
+            OnPut?.Invoke();
+        }
+        public void InsertRow(int rowIndex)
+        {
+            OnInsertRow?.Invoke();
+        }
+        public void InsertColumn(int columnIndex)
+        {
+            OnInsertColumn?.Invoke();
+        }
+        public int Get(int row, int column)
+        {
+            return 0;
+        }
+        
+    }
+}
