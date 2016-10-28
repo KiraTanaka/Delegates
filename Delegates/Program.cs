f4e213fbd970ce662cb62cbe3db2aac5d7160e97
Kira Tanaka <suein_i@mail.ru>
2016-10-28 16:20:38 +0500

    Correction

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -8,24 +8,10 @@ using System.Threading.Tasks;
 
 namespace Delegates
 {
-    public class observer
-    {
-        public void Change(object sender, DataModelPutEventArgs e)
-        {
-            Console.WriteLine(e.ProposedValue);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
-        {
-            DataModel model = new DataModel();
-            observer ob = new observer();
-            model.OnPut += ob.Change;
-            model.InsertRow(0);
-            model.InsertColumn(0);
-            model.InsertColumn(1);
-            model.Put(0,1,4);
+        {            
             Console.ReadLine();
         }
     }
